ceb614b5fb886e131072fdc66cf48330985c94db
Fatmir Qelibari <lgit-kontakt@fatmir.qelibari.de>
2021-05-20 16:25:15 +0200

    First try to read the lines

--- search_element_in_webseite.py
+++ search_element_in_webseite.py
@@ -33,17 +33,43 @@ def get_html_content(website):
     with open("source_code.txt", 'wb') as f:
         f.write(mbytes)
 
+def search_code_for_inforamtion(search_key):
+    with open("source_code.txt") as f:
+        lines = f.read()
+    cmp = []
+    one_char = ""
+    idx = 0
+    max_idx = len(search_key) - 1
+    for line in lines:
+        for ch in line:
+            try:
+                one_char = ch.encode("utf-8")
+            except UnicodeError:
+                print(ch, "Geht nicht")
+
+            if one_char == search_key[idx]:
+                cmp.append(one_char)
+                idx += 1
+            else:
+                cmp = []
+                idx = 0
+                if idx == max_idx:
+                    return cmp
+    return 1
+
+
 
 
 if __name__ == "__main__":
-    sys.argv.append("http://www.google.de")
-    sys.argv.append("hello world")
+    sys.argv.append("https://www.loremipsum.de")
+    sys.argv.append("At vero")
     if len(sys.argv) == 3:
         my_parser = define_new_parser()
         my_parser.parse_args()
         #open_safari()
         get_html_content(sys.argv[1])
         #open_website(sys.argv[1])
+        print(search_code_for_inforamtion(sys.argv[-1]))
 
     elif len(sys.argv) < 3:
         sys.stdout.write("Too few Arguments.")
