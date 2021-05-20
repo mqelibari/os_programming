2a2966a05bed0a9e7d6941f4d83c8450b3b21037
Fatmir Qelibari <lgit-kontakt@fatmir.qelibari.de>
2021-05-20 12:51:51 +0200

    Catch byte code of HTMl

--- search_element_in_webseite.py
+++ search_element_in_webseite.py
@@ -1,7 +1,9 @@
+# -*- coding: UTF-8 -*-
 import argparse
 import webbrowser
 import subprocess
 import sys
+import urllib.request
 import os
 
 def define_new_parser():
@@ -16,17 +18,33 @@ def define_new_parser():
 
 
 def open_safari():
-    subprocess.run(["python3", "./start_programms.py", "TextEdit"])
+    subprocess.run(["python3", "./start_programms.py", "Safari"])
 
 def open_website(website):
-    webbrowser.open_new(website)
+    safari = webbrowser.get("Safari")
+    response = safari.open(website, new=1, autoraise=0)
+    safari.open_new_tab("www.google.de")
+    if not response:
+        sys.stdout.write("Could not open Browser.")
+
+def get_html_content(website):
+    my = urllib.request.urlopen(website)
+    mbytes = my.read()
+    with open("source_code.txt", 'wb') as f:
+        f.write(mbytes)
+
+
 
 if __name__ == "__main__":
+    sys.argv.append("http://www.google.de")
+    sys.argv.append("hello world")
     if len(sys.argv) == 3:
         my_parser = define_new_parser()
         my_parser.parse_args()
-        open_safari()
-        open_website(sys.argv[1])
+        #open_safari()
+        get_html_content(sys.argv[1])
+        #open_website(sys.argv[1])
+
     elif len(sys.argv) < 3:
         sys.stdout.write("Too few Arguments.")
     else:
