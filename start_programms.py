08d454c4be96f2d64dfd5592b474e5a0a7bb66d4
Fatmir Qelibari <lgit-kontakt@fatmir.qelibari.de>
2021-05-20 02:31:57 +0200

    End of Project

--- start_programms.py
+++ start_programms.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python
 import argparse
 import sys
-
+import subprocess
 
 def define_parser_with_arguments():
-    my_parser = argparse.ArgumentParser(description="Open Application.")
-    my_parser.add_argument("-a", "--app", help="Application Name.", type=str, metavar="")
+    my_parser = argparse.ArgumentParser(description="Open Application.", usage="%(prog)s [-h|--help] [App-Name]")
+    my_parser.add_argument("[App-Name]", help="The Name of the Programm you want to start.")
     return my_parser
 
-def check_for_invalid_flags():
-    valid_falgs = ["a", "--app"]
-    for flag in sys.argv[1:]:
-        if flag not in valid_falgs:
-            return 1
+def open_programm():
+    output = subprocess.run(["open", "-a", sys.argv[-1]], stdout=subprocess.PIPE)
+    if output.returncode != 1:
+        return 1
 
 if __name__ == "__main__":
-    invalid_flag_found = check_for_invalid_flags()
-    if invalid_flag_found:
-        sys.exit()
+    if len(sys.argv) > 2:
+        sys.stdout.write("Too many Arguments")
+        exit()
     my_parser = define_parser_with_arguments()
+    args = my_parser.parse_args()
+    fail_to_open = open_programm()
+    if fail_to_open:
+        sys.stdout.write("Unable to open Programm.")
+        exit()
