800e63ad9c3bdcb7ee4c70c79d3dd23dbd793148
Fatmir Qelibari <lgit-kontakt@fatmir.qelibari.de>
2021-05-20 21:08:37 +0200

    Add example Site Name Prize

--- search_element_in_webseite.py
+++ search_element_in_webseite.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 import argparse
-import webbrowser
-import subprocess
 import sys
-import urllib.request
-import os
+from urllib.request import urlopen
 
 def define_new_parser():
     my_parser = argparse.ArgumentParser(description="Which webseite to look for something.", usage="&(prog)s [url]\
@@ -18,59 +15,47 @@ def define_new_parser():
 
 
 
-def open_safari():
-    subprocess.run(["python3", "./start_programms.py", "Safari"])
-
-def open_website(website):
-    safari = webbrowser.get("Safari")
-    response = safari.open(website, new=1, autoraise=0)
-    safari.open_new_tab("www.google.de")
-    if not response:
-        sys.stdout.write("Could not open Browser.")
 
 def get_html_content(website):
-    my = urllib.request.urlopen(website)
+    my = urlopen(website)
+    print(my)
     mbytes = my.read()
     with open("source_code.txt", 'wb') as f:
         f.write(mbytes)
 
-def search_code_for_inforamtion(search_key):
-    with open("source_code.txt") as f:
-        lines = f.read()
-    cmp = []
-    one_char = ""
-    idx = 0
-    max_idx = len(search_key) - 1
-    for line in lines:
-        for ch in line:
-            try:
-                one_char = ch.encode("utf-8")
-            except UnicodeError:
-                print(ch, "Geht nicht")
-
-            if one_char == search_key[idx]:
-                cmp.append(one_char)
-                idx += 1
-            else:
-                cmp = []
-                idx = 0
-                if idx == max_idx:
-                    return cmp
-    return 1
+# def search_code_for_inforamtion(search_key):
+#     with open("source_code.txt") as f:
+#         lines = f.read()
+#     cmp = []
+#     one_char = ""
+#     idx = 0
+#     max_idx = len(search_key) - 1
+#     for char in lines:
+#         try:
+#             one_char = char.encode("utf-8")
+#         except UnicodeError:
+#             print(char, "Geht nicht")
+#         if one_char == search_key[idx]:
+#             cmp.append(one_char)
+#             idx += 1
+#         else:
+#             cmp = []
+#             idx = 0
+#             if idx == max_idx:
+#                 return cmp
+#     return 1
 
 
 
 
 if __name__ == "__main__":
-    sys.argv.append("https://www.loremipsum.de")
-    sys.argv.append("At vero")
+    sys.argv.append("https://www.otto.de/technik/heimkino/")
+    sys.argv.append(["LG DSN4 2.1 2.1 Soundsystem (Bluetooth, 300 W)",
+                     "Panasonic SC-HTB510 2.1 Soundbar (Bluetooth, WLAN (WiFi), 240 W, mit kabellosem Subwoofer)"])
     if len(sys.argv) == 3:
         my_parser = define_new_parser()
         my_parser.parse_args()
-        #open_safari()
         get_html_content(sys.argv[1])
-        #open_website(sys.argv[1])
-        print(search_code_for_inforamtion(sys.argv[-1]))
 
     elif len(sys.argv) < 3:
         sys.stdout.write("Too few Arguments.")
